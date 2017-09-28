cf8ce0caddfa6ae7e97049e52c3bda5b3937c459
Gabriel Chavez <gabriel.chavez@ericsson.com>
2017-09-28 13:08:33 +0300

    Remove elisp-slime-nav and implement the C-c C-d C-d behaviour

--- ged-dev-lisp.el
+++ ged-dev-lisp.el
@@ -8,12 +8,32 @@
   (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
   (add-hook 'ielm-mode-hook 'paredit-mode))
 
-(use-package elisp-slime-nav
+
+;;; Shamelessly copied from
+;;;  https://github.com/purcell/elisp-slime-nav/blob/34938422929800839e2f935aca890cd4a229ca99/elisp-slime-nav.el#L119
+(defun ged--read-elisp-symbol-at-point ()
+  "Return the symbol at point as a string."
+  (let* ((sym-at-point (symbol-at-point))
+         (at-point (and sym-at-point (symbol-name sym-at-point))))
+    at-point))
+
+(defun ged-describe-elisp-at-point (sym-name)
+  (interactive (list (ged--read-elisp-symbol-at-point)))
+  (let ((symbol (intern sym-name)))
+    (if (fboundp symbol)
+        (describe-symbol symbol)
+      (message "No docs"))))
+
+(use-package xref
   :ensure t
   :config
-  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-    (add-hook hook 'elisp-slime-nav-mode)))
-
+  (dolist (hook '(emacs-lisp-mode-hook ielm-mod-ehook))
+    (add-hook hook (lambda ()
+                     (local-set-key (kbd "M-.") 'xref-find-definitions)
+                     (local-set-key (kbd "M-,") 'xref-pop-marker-stack)
+                     (local-set-key (kbd "C-M-.") 'xref-find-definitions-other-window)
+                     (local-set-key (kbd "C-c C-d d") 'ged-describe-elisp-at-point)
+                     (local-set-key (kbd "C-c C-d C-d") 'ged-describe-elisp-at-point)))))
 
 ;;; Scheme settings
 (setq geiser-active-implementations '(guile))
