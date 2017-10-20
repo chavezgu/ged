9abd3f26dd605f240851975b9c682cf9d93663be
Gabriel Chavez <gabriel.chavez@ericsson.com>
2017-10-20 16:28:59 +0300

    No global-hl-line-mode set to true

--- ged-core.el
+++ ged-core.el
@@ -166,9 +166,6 @@
 
 ;; (global-set-key (kbd "C-c C-f") 'jump-char-forward)
 
-;; Because I'm old, I need to highlight the line where my cursor is.
-(global-hl-line-mode 1)
-
 ;;; Add custom lisp of machines
 (when (file-exists-p "~/.emacs.d/site-lisp")
   (add-to-list 'load-path "~/.emacs.d/site-lisp"))
